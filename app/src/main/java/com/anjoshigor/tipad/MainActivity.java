aacadd4fb9bc5cf3fa2aa0a9f88b8b8292432bf7
Higor Anjos <higor.araujo.anjos@gmail.com>
2016-10-05 00:05:11 -0300

    Telas do pancreas de jogo e de perguntas adicionadas

--- app/src/main/java/com/anjoshigor/tipad/MainActivity.java
+++ app/src/main/java/com/anjoshigor/tipad/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anjoshigor.tipad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,43 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Toast toastP, toastH, toastA, toastT;
-
+    Intent intentP;
+    ImageView pancreasImage, hipofiseImage, adrenalImage, tireoideImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Selecione o Assunto");
+        populateImages();
 
-        toastP = Toast.makeText(getApplicationContext(), "BUTAO Pancreas", Toast.LENGTH_LONG);
-        toastH = Toast.makeText(getApplicationContext(), "BUTAO Hipotálamo", Toast.LENGTH_LONG);
-        toastT = Toast.makeText(getApplicationContext(), "BUTAO Tireoide", Toast.LENGTH_LONG);
-        toastA = Toast.makeText(getApplicationContext(), "BUTAO Adrenal", Toast.LENGTH_LONG);
 
+
+    }
+
+    private void populateImages() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                pancreasImage = (ImageView) findViewById(R.id.pancreasImageView);
+                tireoideImage = (ImageView) findViewById(R.id.tireoideImageView);
+                hipofiseImage = (ImageView) findViewById(R.id.hipofiseImageView);
+                adrenalImage = (ImageView) findViewById(R.id.adrenalImageView);
+                hipofiseImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_hipofise_02));
+                tireoideImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_tireoide_02));
+                adrenalImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_adrenal_02));
+                pancreasImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_pancreas_02));
+                toastP = Toast.makeText(getApplicationContext(), "BUTAO Pancreas", Toast.LENGTH_LONG);
+                toastH = Toast.makeText(getApplicationContext(), "BUTAO Hipotálamo", Toast.LENGTH_LONG);
+                toastT = Toast.makeText(getApplicationContext(), "BUTAO Tireoide", Toast.LENGTH_LONG);
+                toastA = Toast.makeText(getApplicationContext(), "BUTAO Adrenal", Toast.LENGTH_LONG);
+                intentP = new Intent(getBaseContext(), Pancreas.class);
+                intentP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
     }
 
     public void callPancreasView(View v) {
         toastP.show();
+        startActivity(intentP);
     }
 
     public void callHipotView(View v) {
---
+++ app/src/main/java/com/anjoshigor/tipad/Pancreas.java
@@ -0,0 +1,55 @@
+package com.anjoshigor.tipad;
+
+/**
+ * Created by anjoshigor on 04/10/16.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Pancreas extends AppCompatActivity {
+
+    Toast toastP, toastC;
+    ImageView playImage, curImage;
+    Intent intentP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pancreas);
+        setTitle("Pâncreas");
+        populateImages();
+
+
+    }
+
+    private void populateImages() {
+        runOnUiThread(new Runnable(){
+            public void run() {
+                playImage= (ImageView)findViewById(R.id.playImageView);
+                playImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_play));
+                curImage= (ImageView)findViewById(R.id.curImageView);
+                curImage.setImageDrawable(getResources().getDrawable(R.drawable.rsz_question_mark));
+                toastP = Toast.makeText(getApplicationContext(), "BUTAO Jogar", Toast.LENGTH_LONG);
+                toastC = Toast.makeText(getApplicationContext(), "BUTAO Curiosidades", Toast.LENGTH_LONG);
+                intentP = new Intent(getBaseContext(), Perguntas.class);
+                intentP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
+    }
+
+    public void callPlayView(View v) {
+        startActivity(intentP);
+        toastP.show();
+    }
+
+    public void callCurView(View v) {
+        toastC.show();
+    }
+
+
+}
