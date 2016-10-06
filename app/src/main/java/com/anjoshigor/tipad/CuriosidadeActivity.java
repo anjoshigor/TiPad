09e34ad7a7f449044e7627edf1e329e5ba56c80f
Higor Anjos <higor.araujo.anjos@gmail.com>
2016-10-06 19:31:10 -0300

    Tela de curiosidades alterada e perguntas sobre pancreas adicionadas | Issue #4

--- app/src/main/java/com/anjoshigor/tipad/CuriosidadeActivity.java
+++ app/src/main/java/com/anjoshigor/tipad/CuriosidadeActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class CuriosidadeActivity extends AppCompatActivity {
-    TextView curTextView;
+    TextView curTextView, doenca1, doenca2, tituloDoenca1, tituloDoenca2;
     String assunto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,34 +14,53 @@ public class CuriosidadeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_curiosidade);
         assunto = getIntent().getStringExtra("assunto");
         setTitle("Curiosidades sobre "+assunto);
-        curTextView = (TextView) findViewById(R.id.curTextView);
-        curTextView.setText(switchAssunto(assunto));
+        curTextView = (TextView) findViewById(R.id.introTextView);
+        doenca1 = (TextView) findViewById(R.id.doencaTextView1);
+        doenca2 = (TextView) findViewById(R.id.doencaTextView2);
+        tituloDoenca1 = (TextView) findViewById(R.id.doencaTituloTextView1);
+        tituloDoenca2 = (TextView) findViewById(R.id.doencaTituloTextView2);
+
+        updateViews(assunto);
     }
 
-    private String switchAssunto(String assunto){
-        String text = new String();
-
-        switch (assunto){
+    private void updateViews(String assunto) {
+        switch (assunto) {
             case "Pâncreas":
-                text = getResources().getString(R.string.curPancreas);
-            break;
+                curTextView.setText(getResources().getString(R.string.introPancreas));
+                doenca1.setText(getResources().getString(R.string.doencaPancreas1));
+                doenca2.setText(getResources().getString(R.string.doencaPancreas2));
+                tituloDoenca1.setText(getResources().getString(R.string.tDoencaPancreas1));
+                tituloDoenca2.setText(getResources().getString(R.string.tDoencaPancreas2));
+                break;
 
             case "Hipófise/Hipotálamo":
-                text = getResources().getString(R.string.curHipot);
-            break;
+                curTextView.setText(getResources().getString(R.string.introHipot));
+                doenca1.setText(getResources().getString(R.string.doencaHipot1));
+                doenca2.setText(getResources().getString(R.string.doencaHipot2));
+                tituloDoenca1.setText(getResources().getString(R.string.tDoencaHipot1));
+                tituloDoenca2.setText(getResources().getString(R.string.tDoencaHipot2));
+                break;
 
             case "Glândula Adrenal":
-                text = getResources().getString(R.string.curAdrenal);
-            break;
+                curTextView.setText(getResources().getString(R.string.introAdrenal));
+                doenca1.setText(getResources().getString(R.string.doencaAdrenal1));
+                doenca2.setText(getResources().getString(R.string.doencaAdrenal2));
+                tituloDoenca1.setText(getResources().getString(R.string.tDoencaAdrenal1));
+                tituloDoenca2.setText(getResources().getString(R.string.tDoencaAdrenal2));
+                break;
 
             case "Tireóide":
-                text = getResources().getString(R.string.curTireoide);
-            break;
+                curTextView.setText(getResources().getString(R.string.introTireoide));
+                doenca1.setText(getResources().getString(R.string.doencaTireoide1));
+                doenca2.setText(getResources().getString(R.string.doencaTireoide2));
+                tituloDoenca1.setText(getResources().getString(R.string.tDoencaTireoide1));
+                tituloDoenca2.setText(getResources().getString(R.string.tDoencaTireoide2));
+                break;
 
             default:
-            break;
+                break;
+
         }
 
-        return text;
     }
 }
